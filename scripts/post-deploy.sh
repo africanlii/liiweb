2d2116f85a13ff6191a47c59456b169a29e89fe9
Greg Kempe <greg@kempe.net>
2020-02-06 15:49:46 +0200

    Docs

--- scripts/post-deploy.sh
+++ scripts/post-deploy.sh
@@ -8,12 +8,20 @@ drush=vendor/drush/drush/drush
 #
 # ref: http://dokku.viewdocs.io/dokku/advanced-usage/deployment-tasks/#appjson-deployment-tasks
 
+# 1. Set the site into maintenance mode, this lowers the chances of DB transaction deadlocks
 $drush state-set system.maintenance_mode TRUE && \
+# 2. Run updatedb #1 (some modules update before the config import)
 $drush updatedb -y && \
+# 3. Clear caches
 $drush cr && \
+# 4. Run config import
 $drush cim sync -y && \
+# 5. Run updatedb #2 (some modules update AFTER the config import)
 $drush updatedb -y && \
+# 6. Update module string translations
 $drush locale:check && \
 $drush locale:update && \
+# 7. Clear caches one more time
 $drush cr && \
+# 8. Remove maintenance mode
 $drush state-set system.maintenance_mode FALSE
