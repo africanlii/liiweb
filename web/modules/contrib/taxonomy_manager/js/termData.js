8088f2d49e96c8134af615b34e94709426e8e7b8
Greg Kempe <greg@kempe.net>
2020-02-06 12:22:58 +0200

    Remove web/modules/contrib/

--- web/modules/contrib/taxonomy_manager/js/termData.js
+++
@@ -1,9 +0,0 @@
-(function ($, Drupal, drupalSettings) {
-  'use strict';
-
-  Drupal.TaxonomyManagerTermData = function (tid, tree) {
-    // We change the hidden form element which then triggers the AJAX system.
-    $('input[name=load-term-data]').val(tid).trigger('change');
-  };
-
-})(jQuery, Drupal, drupalSettings);
