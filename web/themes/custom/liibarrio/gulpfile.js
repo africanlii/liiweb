675c0a63d7b95b0a71fb558bba1b2d45c229400b
Greg Kempe <greg@kempe.net>
2020-04-20 18:06:03 +0200

    Use indigo-web from npm

--- web/themes/custom/liibarrio/gulpfile.js
+++ web/themes/custom/liibarrio/gulpfile.js
@@ -12,6 +12,7 @@ const paths = {
     src: "./scss/style.scss",
     dest: "./css",
     watch: "./scss/**/*.scss",
+    indigo_web: "./node_modules/indigo-web/css/akoma-ntoso.css",
   },
   js: {
     bootstrap: "./node_modules/bootstrap/dist/js/bootstrap.min.js",
@@ -24,7 +25,7 @@ const paths = {
 // Compile sass into CSS & auto-inject into browsers
 function styles() {
   return gulp
-    .src([paths.scss.src])
+    .src([paths.scss.src, paths.scss.indigo_web])
     .pipe(sourcemaps.init())
     .pipe(sass().on("error", sass.logError))
     .pipe(
