9dfffcd16df7017ca16b4e93d306add4109bb525
James Anderton <james.legion@gmail.com>
2020-07-14 16:31:14 +0200

    liibarrio - judgement content type

--- web/themes/custom/liibarrio2020/js/niceScroll.js
+++ web/themes/custom/liibarrio2020/js/niceScroll.js
@@ -3,7 +3,7 @@
 
   Drupal.behaviors.niceScrollTableOfContents = {
     attach: function (context, settings) {
-      $(".table-of-contents").niceScroll({
+      $(".table-of-contents.table-of-contents--main").niceScroll({
         cursorcolor: "#d5d5d5",
         cursorwidth: "10px",
         autohidemode: false,
