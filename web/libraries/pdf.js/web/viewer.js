24c514c77f74e2fd9d5d7d7e3b64e7101c3a64f6
roger-cts <roger.lange@cedartower.co.za>
2020-11-19 15:41:41 +0200

    Add origin for cname and S3 to pdf.js

--- web/libraries/pdf.js/web/viewer.js
+++ web/libraries/pdf.js/web/viewer.js
@@ -1779,7 +1779,7 @@ const PDFViewerApplication = {
 exports.PDFViewerApplication = PDFViewerApplication;
 let validateFileURL;
 {
-  const HOSTED_VIEWER_ORIGINS = ["null", "http://mozilla.github.io", "https://mozilla.github.io"];
+  const HOSTED_VIEWER_ORIGINS = ["null", "http://mozilla.github.io", "https://mozilla.github.io","https://media.namiblii.org/","s3://namiblii-media/"];
 
   validateFileURL = function (file) {
     if (file === undefined) {
