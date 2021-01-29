7e948e3eb92fe7e981818928460458bcc28f757d
roger-cts <roger.lange@cedartower.co.za>
2021-01-29 09:03:49 +0200

    Ulii social and about update

--- web/modules/custom/liiweb/src/Plugin/Block/ContactBlock.php
+++ web/modules/custom/liiweb/src/Plugin/Block/ContactBlock.php
@@ -25,9 +25,10 @@ class ContactBlock extends BlockBase {
 
     //Address
     $address = [
-      'address_1'   => '8th floor, Sanlam Building',
-      'address_2'    => 'Independance Avenue',
-      'address_3'   => 'Windhoek, Namibia',
+      'address_1'   => 'Judicial Training Institute',
+      'address_2'    => 'Plot M105, Kinawataka Road',
+      'address_3'   => 'Mbuya 1, Kampala',
+      'address_4'   => 'Uganda',
     ];
     foreach ($address as $add => $val) {
       $items['address'][] = [
@@ -40,7 +41,7 @@ class ContactBlock extends BlockBase {
 
     //Number
     $numbers = [
-      'fas fa-phone-square' => '+26 (46) 1229 097',
+      'fas fa-phone-square' => '+256 (41) 4286 765',
     ];
 
     foreach ($numbers as $icon => $val) {
@@ -55,16 +56,16 @@ class ContactBlock extends BlockBase {
 
     //website email
     $items['website_email'][] = [
-      '#markup' => '<a href="https://namiblii.org/" target="_blank" class="icon--website list-inline-item" ><i class="fas fa-globe-africa"></i>namiblii.org</a>',
+      '#markup' => '<a href="https://ulii.org/" target="_blank" class="icon--website list-inline-item" ><i class="fas fa-globe-africa"></i>ulii.org</a>',
     ];
     $items['website_email'][] = [
-      '#markup' => '<a href="mailto:info@namiblii.org" class="icon--email list-inline-item" ><i class="fas fa-at"></i>info@namiblii.org</a>',
+      '#markup' => '<a href="mailto:info@ulii.org" class="icon--email list-inline-item" ><i class="fas fa-at"></i>info@ulii.org</a>',
     ];
 
     //Social media
     $platforms = [
-      'fab fa-facebook-square' => 'https://facebook.com/namiblii/',
-      'fab fa-twitter-square'    => 'https://twitter.com/AfricanLII',
+      'fab fa-facebook-square' => 'https://facebook.com/UgandaLII/',
+      'fab fa-twitter-square'    => 'https://twitter.com/Uganda_LII',
     ];
 
     foreach ($platforms as $icon => $url) {
