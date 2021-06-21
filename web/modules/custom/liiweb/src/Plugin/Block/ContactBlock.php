bba1c1ac658a59cc54aaf665fd88ae9732273e5f
roger-cts <roger.lange@cedartower.co.za>
2021-06-21 14:53:18 +0200

    Ghalii minor adjustments

--- web/modules/custom/liiweb/src/Plugin/Block/ContactBlock.php
+++ web/modules/custom/liiweb/src/Plugin/Block/ContactBlock.php
@@ -25,9 +25,10 @@ class ContactBlock extends BlockBase {
 
     //Address
     $address = [
-      'address_1'   => '8th floor, Sanlam Building',
-      'address_2'    => 'Independance Avenue',
-      'address_3'   => 'Windhoek, Namibia',
+      'address_1'   => 'Bank Lane Trade Fair Site, Accra, Ghana',
+      'address_2'    => 'P.O. Box CT 6931',
+      'address_3'   => 'Cantonments',
+      'address_4'   => 'Accra, Ghana',
     ];
     foreach ($address as $add => $val) {
       $items['address'][] = [
@@ -40,7 +41,7 @@ class ContactBlock extends BlockBase {
 
     //Number
     $numbers = [
-      'fas fa-phone-square' => '+26 (46) 1229 097',
+      'fas fa-phone-square' => '+23 354 537 1246',
     ];
 
     foreach ($numbers as $icon => $val) {
@@ -55,15 +56,15 @@ class ContactBlock extends BlockBase {
 
     //website email
     $items['website_email'][] = [
-      '#markup' => '<a href="https://namiblii.org/" target="_blank" class="icon--website list-inline-item" ><i class="fas fa-globe-africa"></i>namiblii.org</a>',
+      '#markup' => '<a href="https://ghalii.org/" target="_blank" class="icon--website list-inline-item" ><i class="fas fa-globe-africa"></i>ghalii.org</a>',
     ];
     $items['website_email'][] = [
-      '#markup' => '<a href="mailto:info@namiblii.org" class="icon--email list-inline-item" ><i class="fas fa-at"></i>info@namiblii.org</a>',
+      '#markup' => '<a href="mailto:info@ghalii.org" class="icon--email list-inline-item" ><i class="fas fa-at"></i>info@ghalii.org</a>',
     ];
 
     //Social media
     $platforms = [
-      'fab fa-facebook-square' => 'https://facebook.com/namiblii/',
+      'fab fa-facebook-square' => 'https://facebook.com/africanlii/',
       'fab fa-twitter-square'    => 'https://twitter.com/AfricanLII',
     ];
 
