9ce2994979b0f997ecd6886ac7ac8eeac2061e4c
Cristian Romanescu <cristian@romanescu.ro>
2021-09-27 09:57:55 +0300

    #157 Cleanup all formatting for banners

--- web/modules/custom/liiweb_legislation/src/LiiWebLegislationUIHelper.php
+++ web/modules/custom/liiweb_legislation/src/LiiWebLegislationUIHelper.php
@@ -39,14 +39,53 @@ class LiiWebLegislationUIHelper {
     $this->liiWebUtils = $liiWebUtils;
   }
 
-  public function formatMessageOlderExpression($fromStr, $toStr, $nid) {
+  /**
+   * Format the banner for the newest version of the legislation.
+   *
+   * @param string $fromStr
+   *   Start date in format YYYY-MM-DD.
+   * @param integer $nid
+   *   Expression node ID.
+   *
+   * @return \Drupal\Core\StringTranslation\TranslatableMarkup|string
+   *   Formatted banner message.
+   */
+  public function formatBannerCurrentExpression($fromStr, $nid) {
+    $default = new TranslatableMarkup('This is the latest version of this legislation');
+    if (empty($fromStr)) {
+      return $default;
+    }
+    $from = \DateTime::createFromFormat('Y-m-d', $fromStr, new \DateTimeZone('UTC'));
+    if (empty($from)) {
+      return $default;
+    }
+    $from->setTime(0, 0);
+    $fromFormatted = $this->dateFormatter->format($from->getTimestamp(), 'short_date');
+    return new TranslatableMarkup('This is the latest version of this legislation commenced on <strong>@from</strong>.', ['@from' => $fromFormatted]);
+  }
+
+  /**
+   * Generate banner message on older expressions of the legislation.
+   *
+   * @param string $fromStr
+   *   Start date in format YYYY-MM-DD.
+   * @param string $toStr
+   *   End date in format YYYY-MM-DD.
+   * @param integer $nid
+   *   Expression node ID.
+   *
+   * @return \Drupal\Core\StringTranslation\TranslatableMarkup|string
+   *   Formatted banner message.
+   */
+  public function formatBannerOlderExpression($fromStr, $toStr, $nid) {
+    $default = new TranslatableMarkup('This is not the latest version of this legislation.');
     if (empty($fromStr) || empty($toStr)) {
-      return '';
+      return $default;
     }
     $from = \DateTime::createFromFormat('Y-m-d', $fromStr, new \DateTimeZone('UTC'));
     $to = \DateTime::createFromFormat('Y-m-d', $toStr, new \DateTimeZone('UTC'));
     if (empty($from) || empty($to)) {
-      return '';
+      return $default;
     }
     $from->setTime(0, 0);
     $to->setTime(0, 0);
@@ -66,7 +105,7 @@ class LiiWebLegislationUIHelper {
       }
       if ($latestRevisionFromFormatted) {
         $link = Link::createFromRoute(
-          new TranslatableMarkup('Read the version currently in force from <strong>@from</strong>.', ['@from' => $latestRevisionFromFormatted]),
+          new TranslatableMarkup('Read the current version commenced on <strong>@from</strong>.', ['@from' => $latestRevisionFromFormatted]),
           'entity.node.canonical', ['node' => $nid]
         )->toString();
       }
@@ -82,6 +121,38 @@ class LiiWebLegislationUIHelper {
     return new TranslatableMarkup(
       'This is the version of this legislation as it was from <strong>@from</strong> to <strong>@to</strong>. <h3 class="h6">@link</h3>',
       ['@from' => $fromFormatted, '@to' => $toFormatted, '@link' => $link]
+    );
+  }
+
+  /**
+   * @param \Drupal\node\NodeInterface $repealWork
+   *   Work that repealed the current legislation.
+   * @param \DateTime $repealDate
+   *   Date of repeal.
+   *
+   * @return \Drupal\Core\StringTranslation\TranslatableMarkup
+   */
+  public function formatBannerRepealedExpression($repealWork, $repealDate) {
+    $default = new TranslatableMarkup('This legislation was repealed.');
+    if (empty($repealDate)) {
+      return $default;
+    }
+    $repealDateFormatted = $this->dateFormatter->format($repealDate->getTimestamp(), 'short_date');
+    try {
+      $link = Link::createFromRoute($repealWork->getTitle(),
+        'entity.node.canonical',
+        ['node' => $repealWork->id()],
+        ['attributes' => ['title' => new TranslatableMarkup('Click to read this legislation')]]
+      )->toString();
+    } catch (\Exception $e) {
+      $link = new TranslatableMarkup('Unknown');
+    }
+    return new TranslatableMarkup(
+      'This legislation was repealed on <strong>@date</strong> by <h3 class="h6">@link</h3>.',
+      [
+        '@date' => $repealDateFormatted,
+        '@link' => $link
+      ]
     );
   }
 
