17ebc7874c435a40de2ded6180b60cd34fabf3cd
roger-cts <58774261+roger-cts@users.noreply.github.com>
2022-03-07 12:42:52 +0200

    Update viewer.js

--- web/libraries/pdf.js/web/viewer.js
+++ web/libraries/pdf.js/web/viewer.js
@@ -1709,7 +1709,7 @@ const PDFViewerApplication = {
 exports.PDFViewerApplication = PDFViewerApplication;
 let validateFileURL;
 {
-  const HOSTED_VIEWER_ORIGINS = ["null", "http://mozilla.github.io", "https://mozilla.github.io"];
+   const HOSTED_VIEWER_ORIGINS = ["null", "http://mozilla.github.io", "https://mozilla.github.io", window.location.protocol + "//" + window.location.host];
 
   validateFileURL = function (file) {
     if (file === undefined) {
